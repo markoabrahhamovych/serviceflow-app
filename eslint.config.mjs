fe672dbdffcf8d66d733b89f26512f5353d62ac0
M.Abrahamovych <marko.abrahamovych@probegin.com>
2025-11-29 18:57:09 +0200

    Add pre-commit actions with husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,5 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier';
 
@@ -17,13 +15,10 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
       prettier,
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
       'prettier/prettier': ['warn'],
       'no-unused-vars': 'off',
       'react-hooks/exhaustive-deps': 'off',
